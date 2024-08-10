901b33e7a5507bc4aaaae4f55e4ce6d0af191349
munozrendonluismiguel <munozrendonluismiguel@gmail.com>
2024-08-10 17:19:49 +0000

    holiiissssss

--- inicio1.py
+++ inicio1.py
@@ -1,9 +1,9 @@
-# estructura-datos
-....
-edad= 22
+edad=22
 
+edad= int(input("ingrese su edad: "))
 #crear una condicion donde los menores de 18 no puedan entrar
-if edad < 18:
+if edad <= 18:
     print("no puedes entrar")
 else:
     print("puedes entrar")
+#edad=2
