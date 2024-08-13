755e28543855daa4116ba7e28dc6b7bd6ebc71f9
luism7p7 <luism.munoz@campusucc.edu.co>
2024-08-13 03:20:29 +0000

    mis practicas de la fecha 12/08/2024

---
+++ ejercicio#8.py
@@ -0,0 +1,11 @@
+numero=int(input('escribe tu primer numero entero porfavor:'))
+numero2=int(input('escribe tu segundo numero entero porfavor:'))
+
+print('este es tu primer numero:',numero,'y este es tu segundo numero:',numero2)
+
+c=numero/numero2
+r=numero-numero2
+
+print( 'el numero',(numero),'entre',numero2,'da un cociente, y este es el resultaso',float(c))
+
+print('y el resto entre',numero,'y',numero2,'el resultado seria:',float(r))
