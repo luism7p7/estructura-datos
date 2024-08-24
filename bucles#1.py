e5bb725c497c53198e26cdd44403314b28155b14
Luis7o7 <munozrendonluismiguel@gmail.com>
2024-08-24 04:06:20 +0000

    clase 23/08/2024

---
+++ bucles#1.py
@@ -0,0 +1,7 @@
+entero=int(input('digita un numero entero'))
+
+for i in range (1,entero+1):
+    if  i  %2!=0:##indica que no es un  numero par
+        print(i)
+
+##ciclo para  mostrar los numeros impares del 1 al infinito
