a9acf10584797dfc7098d3b5fb158365dff93116
Luis7o7 <munozrendonluismiguel@gmail.com>
2024-08-31 00:38:03 +0000

    mmmm

--- clase#3/ejercicio#5.py
+++
@@ -1,9 +0,0 @@
-
-import random
-
-arreglo=[]
-
-for i in range(100):
-    arreglo.append(random.randint(1,100)para_ in range(10)
-    prom=sum(arreglo)/len(arreglo)               )
-    print(arreglo)
